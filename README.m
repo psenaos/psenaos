083b60d81eb3fb19044b824c89513f13fb07409b
psenaos <139928008+psenaos@users.noreply.github.com>
2023-07-18 19:39:29 -0400

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @psenaos
+- 👀 I’m interested in medicine
+- 🌱 I’m currently learning medicine
+- 💞️ I’m looking to collaborate on medicine
+- 📫 How to reach me ...
+
+<!---
+psenaos/psenaos is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
